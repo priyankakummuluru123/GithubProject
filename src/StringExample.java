168674d9c491035a848d18f236e132491304264a
priya <priya@Windows>
2023-11-16 12:47:24 +0530

    First commit

---
+++ src/StringExample.java
@@ -0,0 +1,15 @@
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String str = "priyanka";
+		if(str.length()>2) {
+			System.out.println(str + " is Greater than length of two");
+		}else {
+			System.out.println(str+" is less than length of two");
+		}
+		
+
+	}
+
+}
